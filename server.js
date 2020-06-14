ad94e1ab21c44b9acc1eb342a8d903aae1ee6b5b
frankmagoba <franklinmagoba4@gmail.com>
2020-06-14 17:13:30 +0300

    updates to deploy to heroku changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<loan-manager>'));
+app.use(express.static(__dirname + '/dist/loan-manager'));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/<loan-manager>/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/loan-manager/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

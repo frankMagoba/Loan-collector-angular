0fdbb668827734654c556378017d989de92d6b6a
frankmagoba <franklinmagoba4@gmail.com>
2020-06-15 15:52:46 +0300

    Run the app by serving the static files in the dist directory

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/loan-manager'));
-
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/loan-manager/index.html'));
-// });
-app.use(express.static(${__dirname}/loan-manager/dist/));
-
-app.get('*', (req, res) => {
-    res.sendFile(`./loan-manager/dist/index.html`);
-     // load the single view file (angular will handle the page changes on the front-end)
-});
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

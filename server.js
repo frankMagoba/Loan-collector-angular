90d0ee6574edd6100ce480cbbc5fedd09c5aaa70
frankmagoba <franklinmagoba4@gmail.com>
2020-06-15 15:47:43 +0300

    try new syntax on server js

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/loan-manager'));
+// app.use(express.static(__dirname + '/dist/loan-manager'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/loan-manager/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/loan-manager/index.html'));
+// });
+app.use(express.static(${__dirname}/loan-manager/dist/));
+
+app.get('*', (req, res) => {
+    res.sendFile(`./loan-manager/dist/index.html`);
+     // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port

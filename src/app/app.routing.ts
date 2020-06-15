2a29e44c85dd00581a80d27af60c1f7eeead4388
frankmagoba <franklinmagoba4@gmail.com>
2020-06-15 17:17:37 +0300

    loans component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,13 @@
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { AuthGuard } from './_helpers';
+import { LoansComponent } from './loans/loans.component'
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
+    { path: 'loans', component: LoansComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }

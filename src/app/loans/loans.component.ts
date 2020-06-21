14f01ddf17a739e65872d0756aafbbb792fb367c
frankmagoba <franklinmagoba4@gmail.com>
2020-06-21 16:12:52 +0300

    migrate from less to scss

--- src/app/loans/loans.component.ts
+++ src/app/loans/loans.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loans',
   templateUrl: './loans.component.html',
-  styleUrls: ['./loans.component.less']
+  styleUrls: ['./loans.component.scss']
 })
 export class LoansComponent implements OnInit {
 
